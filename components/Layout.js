3f393e8695ae55abf56f2c53c085cf8925ebb728
vasundharaux <75647666+vasundharaux@users.noreply.github.com>
2025-11-21 14:42:13 -0800

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,18 @@
-import Header from "./Header";
 import NavBar from "./NavBar";
 
 export default function Layout({ children }) {
   return (
-    <div className="app-root">
-      <Header />
-      <NavBar />
-      <main className="app-shell">{children}</main>
+    <div>
+      {/* Floating Glow Elements */}
+      <div className="glow glow-1"></div>
+      <div className="glow glow-2"></div>
+      <div className="glow glow-3"></div>
+
+      {/* Main Content */}
+      <div id="page-wrapper">
+        <NavBar />
+        <main>{children}</main>
+      </div>
     </div>
   );
 }
